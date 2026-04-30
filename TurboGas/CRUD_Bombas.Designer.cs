82c70a63c8d47573d7126f91d4015ec6b4758a37
Carlos Lescieur <Lescieurmargarita70@outlook.com>
2026-04-29 22:25:57 -0600

    Visualizacion de CRUD de Bombas y Islas

--- TurboGas/CRUD_Bombas.Designer.cs
+++ TurboGas/CRUD_Bombas.Designer.cs
@@ -28,20 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textbox1 = new TurboGas.Elementos.textbox();
             this.botón1 = new TurboGas.Elementos.Botón();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.textbox2 = new TurboGas.Elementos.textbox();
+            this.textbox3 = new TurboGas.Elementos.textbox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // textbox1
-            // 
-            this.textbox1.BackColor = System.Drawing.Color.White;
-            this.textbox1.Location = new System.Drawing.Point(186, 66);
-            this.textbox1.Name = "textbox1";
-            this.textbox1.Padding = new System.Windows.Forms.Padding(10);
-            this.textbox1.Size = new System.Drawing.Size(200, 49);
-            this.textbox1.TabIndex = 0;
             // 
             // botón1
             // 
@@ -51,7 +49,7 @@
             this.botón1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(165)))), ((int)(((byte)(125)))));
             this.botón1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.botón1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.botón1.Location = new System.Drawing.Point(393, 514);
+            this.botón1.Location = new System.Drawing.Point(50, 66);
             this.botón1.Name = "botón1";
             this.botón1.Size = new System.Drawing.Size(150, 40);
             this.botón1.TabIndex = 1;
@@ -61,29 +59,110 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 80);
+            this.label1.Font = new System.Drawing.Font("Poppins", 8.25F);
+            this.label1.Location = new System.Drawing.Point(261, 56);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 20);
+            this.label1.Size = new System.Drawing.Size(208, 30);
             this.label1.TabIndex = 2;
-            this.label1.Text = "jeje";
+            this.label1.Text = "Isla a la que pertenece";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(42, 207);
+            this.label2.Font = new System.Drawing.Font("Poppins", 8.25F);
+            this.label2.Location = new System.Drawing.Point(261, 111);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 20);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 30);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Numero";
+            // 
+            // textbox2
+            // 
+            this.textbox2.BackColor = System.Drawing.Color.White;
+            this.textbox2.Location = new System.Drawing.Point(475, 102);
+            this.textbox2.Name = "textbox2";
+            this.textbox2.Padding = new System.Windows.Forms.Padding(10);
+            this.textbox2.Size = new System.Drawing.Size(200, 49);
+            this.textbox2.TabIndex = 3;
+            // 
+            // textbox3
+            // 
+            this.textbox3.BackColor = System.Drawing.Color.White;
+            this.textbox3.Location = new System.Drawing.Point(475, 271);
+            this.textbox3.Name = "textbox3";
+            this.textbox3.Padding = new System.Windows.Forms.Padding(10);
+            this.textbox3.Size = new System.Drawing.Size(200, 49);
+            this.textbox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poppins", 8.25F);
+            this.label3.Location = new System.Drawing.Point(261, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(208, 30);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Isla a la que pertenece";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Poppins", 8.25F);
+            this.label4.Location = new System.Drawing.Point(261, 281);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 30);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Numero";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(476, 56);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(199, 28);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(476, 225);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(199, 28);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(476, 338);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(199, 28);
+            this.comboBox3.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Poppins", 8.25F);
+            this.label5.Location = new System.Drawing.Point(261, 338);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 30);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Actividad";
             // 
             // CRUD_Bombas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textbox3);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.textbox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.botón1);
-            this.Controls.Add(this.textbox1);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "CRUD_Bombas";
             this.Size = new System.Drawing.Size(900, 625);
@@ -93,10 +172,16 @@
         }
 
         #endregion
-
-        private Elementos.textbox textbox1;
         private Elementos.Botón botón1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private Elementos.textbox textbox2;
+        private Elementos.textbox textbox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label5;
     }
 }
--- TurboGas/CRUD_Islas.Designer.cs
+++ TurboGas/CRUD_Islas.Designer.cs
@@ -28,18 +28,236 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCreate = new TurboGas.Elementos.Botón();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbCreSucursal = new System.Windows.Forms.ComboBox();
+            this.cmbUpdSuc = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnUpd = new TurboGas.Elementos.Botón();
+            this.cmbUpdBomba = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbDelBomb = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnDel = new TurboGas.Elementos.Botón();
+            this.roundPanel1 = new TurboGas.Elementos.RoundPanel();
+            this.roundPanel2 = new TurboGas.Elementos.RoundPanel();
+            this.roundPanel3 = new TurboGas.Elementos.RoundPanel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.roundPanel1.SuspendLayout();
+            this.roundPanel2.SuspendLayout();
+            this.roundPanel3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(120)))), ((int)(((byte)(59)))));
+            this.btnCreate.FlatAppearance.BorderSize = 0;
+            this.btnCreate.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(77)))), ((int)(((byte)(0)))));
+            this.btnCreate.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(165)))), ((int)(((byte)(125)))));
+            this.btnCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCreate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnCreate.Location = new System.Drawing.Point(165, 97);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(150, 40);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Crear";
+            this.btnCreate.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 8.25F);
+            this.label1.Location = new System.Drawing.Point(9, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sucursal a la que pertenece";
+            // 
+            // cmbCreSucursal
+            // 
+            this.cmbCreSucursal.FormattingEnabled = true;
+            this.cmbCreSucursal.Location = new System.Drawing.Point(271, 49);
+            this.cmbCreSucursal.Name = "cmbCreSucursal";
+            this.cmbCreSucursal.Size = new System.Drawing.Size(121, 28);
+            this.cmbCreSucursal.TabIndex = 3;
+            // 
+            // cmbUpdSuc
+            // 
+            this.cmbUpdSuc.FormattingEnabled = true;
+            this.cmbUpdSuc.Location = new System.Drawing.Point(271, 55);
+            this.cmbUpdSuc.Name = "cmbUpdSuc";
+            this.cmbUpdSuc.Size = new System.Drawing.Size(121, 28);
+            this.cmbUpdSuc.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins", 8.25F);
+            this.label2.Location = new System.Drawing.Point(13, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(252, 30);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Sucursal a la que pertenece";
+            // 
+            // btnUpd
+            // 
+            this.btnUpd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(120)))), ((int)(((byte)(59)))));
+            this.btnUpd.FlatAppearance.BorderSize = 0;
+            this.btnUpd.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(77)))), ((int)(((byte)(0)))));
+            this.btnUpd.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(165)))), ((int)(((byte)(125)))));
+            this.btnUpd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnUpd.Location = new System.Drawing.Point(165, 157);
+            this.btnUpd.Name = "btnUpd";
+            this.btnUpd.Size = new System.Drawing.Size(150, 40);
+            this.btnUpd.TabIndex = 4;
+            this.btnUpd.Text = "Actualizar";
+            this.btnUpd.UseVisualStyleBackColor = false;
+            // 
+            // cmbUpdBomba
+            // 
+            this.cmbUpdBomba.FormattingEnabled = true;
+            this.cmbUpdBomba.Location = new System.Drawing.Point(271, 13);
+            this.cmbUpdBomba.Name = "cmbUpdBomba";
+            this.cmbUpdBomba.Size = new System.Drawing.Size(121, 28);
+            this.cmbUpdBomba.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poppins", 8.25F);
+            this.label3.Location = new System.Drawing.Point(13, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 30);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Bomba";
+            // 
+            // cmbDelBomb
+            // 
+            this.cmbDelBomb.FormattingEnabled = true;
+            this.cmbDelBomb.Location = new System.Drawing.Point(271, 44);
+            this.cmbDelBomb.Name = "cmbDelBomb";
+            this.cmbDelBomb.Size = new System.Drawing.Size(121, 28);
+            this.cmbDelBomb.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Poppins", 8.25F);
+            this.label4.Location = new System.Drawing.Point(9, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 30);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Bomba";
+            // 
+            // btnDel
+            // 
+            this.btnDel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(120)))), ((int)(((byte)(59)))));
+            this.btnDel.FlatAppearance.BorderSize = 0;
+            this.btnDel.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(77)))), ((int)(((byte)(0)))));
+            this.btnDel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(165)))), ((int)(((byte)(125)))));
+            this.btnDel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnDel.Location = new System.Drawing.Point(165, 88);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(150, 40);
+            this.btnDel.TabIndex = 9;
+            this.btnDel.Text = "Eliminar";
+            this.btnDel.UseVisualStyleBackColor = false;
+            // 
+            // roundPanel1
+            // 
+            this.roundPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.roundPanel1.Controls.Add(this.btnCreate);
+            this.roundPanel1.Controls.Add(this.label1);
+            this.roundPanel1.Controls.Add(this.cmbCreSucursal);
+            this.roundPanel1.Location = new System.Drawing.Point(229, 33);
+            this.roundPanel1.Name = "roundPanel1";
+            this.roundPanel1.Size = new System.Drawing.Size(415, 175);
+            this.roundPanel1.TabIndex = 12;
+            // 
+            // roundPanel2
+            // 
+            this.roundPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.roundPanel2.Controls.Add(this.label5);
+            this.roundPanel2.Controls.Add(this.comboBox1);
+            this.roundPanel2.Controls.Add(this.btnUpd);
+            this.roundPanel2.Controls.Add(this.label2);
+            this.roundPanel2.Controls.Add(this.cmbUpdSuc);
+            this.roundPanel2.Controls.Add(this.label3);
+            this.roundPanel2.Controls.Add(this.cmbUpdBomba);
+            this.roundPanel2.Location = new System.Drawing.Point(229, 230);
+            this.roundPanel2.Name = "roundPanel2";
+            this.roundPanel2.Size = new System.Drawing.Size(410, 215);
+            this.roundPanel2.TabIndex = 13;
+            // 
+            // roundPanel3
+            // 
+            this.roundPanel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.roundPanel3.Controls.Add(this.btnDel);
+            this.roundPanel3.Controls.Add(this.label4);
+            this.roundPanel3.Controls.Add(this.cmbDelBomb);
+            this.roundPanel3.Location = new System.Drawing.Point(229, 462);
+            this.roundPanel3.Name = "roundPanel3";
+            this.roundPanel3.Size = new System.Drawing.Size(410, 151);
+            this.roundPanel3.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Poppins", 8.25F);
+            this.label5.Location = new System.Drawing.Point(16, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 30);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Activa";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(271, 92);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 10;
             // 
             // CRUD_Islas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.roundPanel3);
+            this.Controls.Add(this.roundPanel2);
+            this.Controls.Add(this.roundPanel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "CRUD_Islas";
-            this.Size = new System.Drawing.Size(800, 500);
+            this.Size = new System.Drawing.Size(900, 625);
+            this.roundPanel1.ResumeLayout(false);
+            this.roundPanel1.PerformLayout();
+            this.roundPanel2.ResumeLayout(false);
+            this.roundPanel2.PerformLayout();
+            this.roundPanel3.ResumeLayout(false);
+            this.roundPanel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Elementos.Botón btnCreate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbCreSucursal;
+        private System.Windows.Forms.ComboBox cmbUpdSuc;
+        private System.Windows.Forms.Label label2;
+        private Elementos.Botón btnUpd;
+        private System.Windows.Forms.ComboBox cmbUpdBomba;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbDelBomb;
+        private System.Windows.Forms.Label label4;
+        private Elementos.Botón btnDel;
+        private Elementos.RoundPanel roundPanel1;
+        private Elementos.RoundPanel roundPanel2;
+        private Elementos.RoundPanel roundPanel3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
